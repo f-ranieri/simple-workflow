51e96528bf6b7a92f50f041fc789fb85b2654cc3
franieri <francesco.ranieri@objectway.it>
2018-08-24 10:52:11 +0200

    added basic test functionality

--- src/test/java/it/ranieri/pocs/AppTest.java
+++ src/test/java/it/ranieri/pocs/AppTest.java
@@ -1,38 +1,20 @@
 package it.ranieri.pocs;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-config/context-spring-beans.xml")
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Autowired
+	private HelloWorld helloWorld;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void testOrderService() {
+		helloWorld.printHello();
+	}
 }
